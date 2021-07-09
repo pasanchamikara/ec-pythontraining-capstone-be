94cfb0d5ddb5ddc1faeee7e45d9e3251fd79b654
Pasan Chamikara <gwpc92@gmail.com>
2021-07-09 12:02:11 +0530

    = mark removed to fix a model bug

--- items/models.py
+++ items/models.py
@@ -3,6 +3,6 @@ from django.db import models
 # Create your models here.
 
 class Item(models.Model):
-    name = models.CharField(=max_length=50)
+    name = models.CharField(max_length=50)
     description = models.CharField(max_length=50)
     price = models.FloatField()
