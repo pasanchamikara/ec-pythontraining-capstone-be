55ec244df18c22d0d277cdefad83f44cbdb7f0e8
Pasan Chamikara <gwpc92@gmail.com>
2021-07-09 14:38:24 +0530

    capstone get method

--- items/views.py
+++ items/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from rest_framework.views import APIView
-from django.http import HttpResponse
+from django.http import HttpResponse, JsonResponse
 import json
 
 from .models import Item
@@ -13,11 +13,16 @@ from .serializers import ItemSerializer
 class ItemView(APIView):
     def get(self, request):
         try:
-            items = Item.objects.all()
-            items_serializer = ItemSerializer(items)
-            return HttpResponse(json.dumps(items_serializer), headers = {'Content-Type': 'application/json'})
-        except(e):
-            print(e)
+            items = list(Item.objects.values())
+            # print(json.dumps(items))
+            items_json = json.dumps(items)
+            print(type(items_json))
+            # items_serializer = ItemSerializer(items)
+            # return JsonResponse({"data": json.loads(items_json)} ,  safe=False)
+            return JsonResponse(json.loads(items_json) ,  safe=False)
+
+        except Exception as e:
+            print(str(e))
             return HttpResponse(json.dumps({"message": "error while retrieving data"}), headers = {'Content-Type': 'application/json'})
     def post(self, request):
         item = Item(name=request.data["name"], price=request.data["price"], description=request.data["description"])
@@ -29,19 +34,20 @@ class ItemView(APIView):
     
     
 class ItemParameterView(APIView):
-    def get(self, request):
+    def get(self, request, itemId):
         try:
-            items = Item.objects.filter(id=request.data['itemId'])
-            if not items:
-                return HttpResponse(json.dumps({"message": "item not found"}), status = 404, headers = {"Content-Type": "application/json"})
-            else:
-                item_serializer = ItemSerializer(items)
-                return HttpResponse(json.dumps(item_serializer), status = 200, headers = {"Content-Type": "application/json"})
-        except:
-            return HttpResponse(json.dumps({"itemId"}))
-    def put(self, request):
+            items = Item.objects.filter(id=itemId).values()
+
+            items_dict = list(items)[0]
+        
+            return HttpResponse(json.dumps(items_dict), status = 200, headers = {"Content-Type": "application/json"})
+        except IndexError:
+             return HttpResponse(json.dumps({"message": "item not found"}), status = 404, headers = {"Content-Type": "application/json"})
+        except(e):
+            return HttpResponse(json.dumps({"itemId": itemId}), headers = { "Content-Type": "application/json"})
+    def put(self, request, itemId):
         try:
-            items = Item.objects.filter(id=request.data['itemId'])
+            items = Item.objects.filter(id=itemId)
             if not items:
                 return HttpResponse(json.dumps({"message": "item not found"}), status = 404, headers = {"Content-Type": "application/json"})
             else:
@@ -54,20 +60,20 @@ class ItemParameterView(APIView):
                 items.save()
                 return HttpResponse(json.dumps({
                     "message": "content updated",
-                    "itemId" : request.data['itemId']
+                    "itemId" : itemId
                 }), status=200, headers= {"Content-Type": "application/json"})
         except:
             return HttpResponse(json.dumps({
                 "message": "error while updating the content"
             }), headers={"Content-Type": "application/json"})
                 
-    def delete(self, request):
+    def delete(self, request, itemId):
         try:
-            items = Item.objects.filter(id=request.data['itemId'])
+            items = Item.objects.filter(id=itemId)
             if not items:
                 return HttpResponse(json.dumps({"message": "item not found"}), status = 404, headers = {"Content-Type": "application/json"})
             else:
                 items.delete()
-                return HttpResponse(json.dumps({"message":"record deleted successfully", "itemId": request.data['itemId']}), status = 200, headers = {"Content-Type": "application/json"})
+                return HttpResponse(json.dumps({"message":"record deleted successfully", "itemId": itemId}), status = 200, headers = {"Content-Type": "application/json"})
         except:
-            return HttpResponse(json.dumps({"itemId"}))
+            return HttpResponse(json.dumps({"message": "error while deleting the record"}), status=404)
